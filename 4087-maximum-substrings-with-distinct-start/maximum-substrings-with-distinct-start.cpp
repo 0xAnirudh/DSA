aa66deff0fd8eb8ce2027124d1d8d724ef759217
Anirudh! <anirudhchourey5@gmail.com>
2026-06-15 23:44:25 +0530

    Time: 8 ms (79.83%) | Memory: 23.9 MB (80.90%) - LeetSync

---
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -0,0 +1,16 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    int maxDistinct(string s) {
+        int ans = 0;
+        int cnt[26]{};
+        for (char c : s) {
+            if (++cnt[c - 'a'] == 1) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
