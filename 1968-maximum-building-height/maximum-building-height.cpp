045bb88315ba5d315c300d219742cb295079827b
Anirudh! <anirudhchourey5@gmail.com>
2026-06-20 14:07:24 +0530

    Time: 100 ms (11.11%) | Memory: 106.6 MB (84.72%) - LeetSync

---
+++ 1968-maximum-building-height/maximum-building-height.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxBuilding(int n, vector<vector<int>>& restrictions) {
+        restrictions.push_back({1, 0});
+        sort(restrictions.begin(), restrictions.end());
+        int m = restrictions.size();
+        
+        for (int i = 1; i < m; ++i) {
+            int d = restrictions[i][0] - restrictions[i - 1][0];
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i - 1][1] + d);
+        }
+        
+        for (int i = m - 2; i >= 0; --i) {
+            int d = restrictions[i + 1][0] - restrictions[i][0];
+            restrictions[i][1] = min(restrictions[i][1], restrictions[i + 1][1] + d);
+        }
+        
+        int ans = 0;
+        for (int i = 1; i < m; ++i) {
+            int id1 = restrictions[i - 1][0], h1 = restrictions[i - 1][1];
+            int id2 = restrictions[i][0], h2 = restrictions[i][1];
+            ans = max(ans, (h1 + h2 + (id2 - id1)) / 2);
+        }
+        
+        ans = max(ans, restrictions.back()[1] + (n - restrictions.back()[0]));
+        return ans;
+    }
+};
