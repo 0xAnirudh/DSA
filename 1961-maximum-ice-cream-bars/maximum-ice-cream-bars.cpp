da1e1c9710c18a0d0744964b7850918107b95d97
Anirudh! <anirudhchourey5@gmail.com>
2026-06-22 01:16:09 +0530

    Time: 32 ms (59.81%) | Memory: 80.1 MB (89.49%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        ranges::sort(costs);
+        // ranges::sort(costs);
+        sort(costs.begin(), costs.end());
 
         for (int i = 0; i < costs.size(); ++i)
             if (coins >= costs[i])
